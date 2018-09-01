f6f874aaf56c3e1306c1930ff401fc9f24b026b0
Arturo Aguila <agu09001@byui.edu>
2018-09-01 15:10:52 -0700

    initial project creation

---
+++ src/main/application/ManaScrewedStudyApp.java
@@ -0,0 +1,10 @@
+package main.application;
+
+/**
+ * Created by Arturo Aguila 9/1/18
+ *
+ * This small project is mean to figure out the best way to shuffle a commander deck
+ * to not get mana screwed, meaning you're short on mana.
+ */
+public class ManaScrewedStudyApp {
+}
