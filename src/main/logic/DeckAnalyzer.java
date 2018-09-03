ef87453c9b3dda49e0ddf427c5d46ad4b13b090d
Arturo Aguila <arturoaguila@Artemis.local>
2018-09-03 15:05:19 -0700

    Add logic layer. Stubbed methods. Initial creation.

---
+++ src/main/logic/DeckAnalyzer.java
@@ -0,0 +1,14 @@
+package main.logic;
+
+import main.model.Deck;
+
+public class DeckAnalyzer {
+
+    /**
+     * Analyze Deck
+     * @param deck
+     */
+    void analyzeDeck(Deck deck){
+        // stub
+    }
+}
---
+++ src/main/logic/Shuffler.java
@@ -0,0 +1,16 @@
+package main.logic;
+
+import main.model.Deck;
+
+public class Shuffler {
+
+    /**
+     * Power shuffle
+     * @param deckOfCards
+     * @param numberOfPiles
+     * @param isRandomPickup
+     */
+    void powerShuffle(Deck deckOfCards, int numberOfPiles, boolean isRandomPickup){
+        // stub
+    }
+}
