b7bff13dec50c116da5f61f4144986fbe43174aa
Arturo Aguila <churro777@gmail.com>
2018-09-03 20:11:24 -0700

    Add javadoc and change return type
    Originally I had this just return void thinking that I would print out
    what was going on.
    
    searchForManaClumps will search for the biggest mana
    clump and return it's size. I think I'm gonna refactor this.
    
    analyzeDeck needs a score. I think I have figured out a good way to
    determine if a deck is randomized enough. I will find the biggest
    mana clump and compare it to the amount of mana in the deck returning
    a double that will represent a percentage. This works well enough but
    I would like to also figure out a way to repersent all the mana in the
    deck not just the biggest mana clump.

--- src/main/logic/DeckAnalyzer.java
+++ src/main/logic/DeckAnalyzer.java
@@ -2,21 +2,27 @@ package main.logic;
 
 import main.model.Deck;
 
+import java.util.Map;
+
 public class DeckAnalyzer {
 
     /**
      * Analyze Deck
      * @param deck
+     * @return score - biggest mana clump/total mana
      */
-    void analyzeDeck(Deck deck){
-        // stub
+    public Double analyzeDeck(Deck deck){
+        //
+        return 0.0;
     }
 
     /**
      * Search for Mana Clumps
      * @param deck
+     * @return biggest mana clump - goal is around 3
      */
-    void searchForManaClumps(Deck deck){
+    public int searchForManaClumps(Deck deck){
         // stub
+        return 35;
     }
 }
