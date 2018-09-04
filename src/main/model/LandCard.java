ea678888d7563121f97a18a7306954da1909e689
Arturo Aguila <churro777@gmail.com>
2018-09-03 20:09:59 -0700

    Add default constructor

--- src/main/model/LandCard.java
+++ src/main/model/LandCard.java
@@ -3,6 +3,10 @@ package main.model;
 public class LandCard implements Card {
     private String landType;
 
+    public LandCard(){
+        super();
+    }
+
     public LandCard(String landType){
         super();
         this.landType = landType;
