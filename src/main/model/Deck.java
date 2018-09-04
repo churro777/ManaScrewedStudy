5a09c8a7e332e8eaea4e2a2ba034baa4364f96f2
Arturo Aguila <churro777@gmail.com>
2018-09-03 20:07:31 -0700

    Add javadocs and new functions
    Non-default constructor - adds cards for you, just input number of lands and nonlands
    removeCardLot() - remove the top X amount of cards
    Getter and setter for numOfLands

--- src/main/model/Deck.java
+++ src/main/model/Deck.java
@@ -4,55 +4,122 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Deck {
-    Deque<Card> cards = new ArrayDeque<>();
+    private Deque<Card> cards = new ArrayDeque<>();
+    private int numOfLands;
 
-    void addCard(Card card){
+    /**
+     * DEFAULT CONSTRUCTOR
+     */
+    public Deck() {
+        super();
+    }
+
+    /**
+     * NON-DEFAULT CONSTRUCTOR
+     * - input how many lands and nonLands to put in
+     * @param numOfLands
+     * @param numOfNonLands
+     */
+    public Deck(int numOfLands, int numOfNonLands) {
+        setNumOfLands(numOfLands);
+        addCardLot(new LandCard(), numOfLands);
+        addCardLot(new NonLandCard(), numOfNonLands);
+    }
+
+    /**
+     * Add a card to the deque
+     * @param card
+     */
+    void addCard(Card card) {
         cards.add(card);
     }
 
-    Card removeTopCard(){
+    /**
+     * Get and remove the top card
+     * @return
+     */
+    Card popTopCard() {
         return cards.pop();
     }
 
-    void addCardLot(Card cardType, int amountOfCards){
+    /**
+     * Add an X amount of cards
+     * @param cardType
+     * @param amountOfCards
+     */
+    void addCardLot(Card cardType, int amountOfCards) {
         for (int i = 0; i < amountOfCards; i++) {
             addCard(cardType);
         }
     }
 
-    int numOfNonLandsAtTop(){
+    /**
+     * Remove X cards from the deck
+     * @param amountOfCards
+     */
+    void removeCardLot(int amountOfCards){
+        for (int i = 0; i < amountOfCards; i++) {
+            cards.pop();
+        }
+    }
+
+    /**
+     * Check how many Non lands are at the top of the deck
+     * @return How many non lands are at the top
+     */
+    int numOfNonLandsAtTop() {
         int numOfNonLands = 0;
 
         Deque<Card> tempCards = cards;
 
-        while(tempCards.size() > 0){
+        while (tempCards.size() > 0) {
             Card card = tempCards.pop();
 
-            if (card.isNonLand()){
+            if (card.isNonLand()) {
                 numOfNonLands++;
             }
-            if (card.isLand()){
+            if (card.isLand()) {
                 return numOfNonLands;
             }
         }
-        return  numOfNonLands;
+        return numOfNonLands;
     }
 
-    int numOfLandsAtTop(){
+    /**
+     * Check how many Lands are at the top of the deck
+     * @return How many Lands are at the top
+     */
+    int numOfLandsAtTop() {
         int numOfLands = 0;
 
         Deque<Card> tempCards = cards;
 
-        while(tempCards.size() > 0){
+        while (tempCards.size() > 0) {
             Card card = tempCards.pop();
 
-            if (card.isLand()){
+            if (card.isLand()) {
                 numOfLands++;
             }
-            if (card.isNonLand()){
+            if (card.isNonLand()) {
                 return numOfLands;
             }
         }
-        return  numOfLands;
+        return numOfLands;
+    }
+
+    /**
+     * Get the amount of lands in the deck
+     * @return amount
+     */
+    public int getNumOfLands() {
+        return numOfLands;
+    }
+
+    /**
+     * Set amount of lands in the deck
+     * @param numOfLands
+     */
+    public void setNumOfLands(int numOfLands) {
+        this.numOfLands = numOfLands;
     }
 }
